db8408569e7ba2dfe7d43a2e57237df12d3e941d
Kayky Casagrande <kbelleboni@gmail.com>
2023-05-30 16:43:58 -0300

    some language feat

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,30 @@ enum BracketState {
     Close,
 }
 
+enum BracketError {
+    UnexpectedClose(char),
+    UnexpectedOpen(char),
+}
+
+impl Display for BracketError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            BracketError::UnexpectedClose(c) => write!(f, "Unexpected close bracket: {}", c),
+            BracketError::UnexpectedOpen(c) => write!(f, "Unexpected open bracket: {}", c),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum TokenType {
     Paren(BracketState),
     Curly(BracketState),
     Square(BracketState),
+    Let,
+    Fn,
+    Colon,
+    Arrow,
+    Assign,
     Comma,
     Dot,
     Minus,
@@ -16,9 +35,12 @@ enum TokenType {
     Semicolon,
     Slash,
     Star,
-    Eof,
-    Word,
+    Ident,
     Integer,
+    Bigger,
+    Smaller,
+    Mut,
+    Eof,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -52,6 +74,8 @@ impl Lexer<'_> {
     }
 }
 
+use std::fmt::Display;
+
 use TokenType::*;
 
 impl<'a> Iterator for Lexer<'a> {
@@ -59,8 +83,15 @@ impl<'a> Iterator for Lexer<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.position >= self.source.len() {
-            if !self.braces_stack.is_empty() {
-                panic!("Unexpected EOF");
+            if let Some(brace) = self.braces_stack.pop() {
+                let brace_error = match brace {
+                    Paren(_) => BracketError::UnexpectedOpen('('),
+                    Curly(_) => BracketError::UnexpectedOpen('{'),
+                    Square(_) => BracketError::UnexpectedOpen('['),
+                    _ => unreachable!(),
+                };
+
+                panic!("{}", brace_error);
             }
 
             return None;
@@ -83,7 +114,7 @@ impl<'a> Iterator for Lexer<'a> {
                 if let Some(Paren(BracketState::Open)) = self.braces_stack.pop() {
                     token
                 } else {
-                    panic!("Unexpected ')'");
+                    panic!("{}", BracketError::UnexpectedClose(')'));
                 }
             }
             b'{' => {
@@ -96,7 +127,7 @@ impl<'a> Iterator for Lexer<'a> {
                 if let Some(Curly(BracketState::Open)) = self.braces_stack.pop() {
                     token
                 } else {
-                    panic!("Unexpected '}}'");
+                    panic!("{}", BracketError::UnexpectedClose('}'));
                 }
             }
             b'[' => {
@@ -109,23 +140,47 @@ impl<'a> Iterator for Lexer<'a> {
                 if let Some(Square(BracketState::Open)) = self.braces_stack.pop() {
                     token
                 } else {
-                    panic!("Unexpected ']'");
+                    panic!("{}", BracketError::UnexpectedClose(']'));
                 }
             }
+            b'<' => Token::new(Smaller, &slice[..1]),
+            b'>' => Token::new(Bigger, &slice[..1]),
             b',' => Token::new(Comma, &slice[..1]),
             b'.' => Token::new(Dot, &slice[..1]),
-            b'-' => Token::new(Minus, &slice[..1]),
+            b'-' => {
+                let old = self.position;
+                self.position += 1;
+                if let Some(Token {
+                    token_type: Bigger, ..
+                }) = self.next()
+                {
+                    Token::new(Arrow, &slice[..2])
+                } else {
+                    self.position = old;
+                    Token::new(Minus, &slice[..1])
+                }
+            }
             b'+' => Token::new(Plus, &slice[..1]),
             b';' => Token::new(Semicolon, &slice[..1]),
             b'/' => Token::new(Slash, &slice[..1]),
             b'*' => Token::new(Star, &slice[..1]),
+            b'=' => Token::new(Assign, &slice[..1]),
+            b':' => Token::new(Colon, &slice[..1]),
             b'\0' => Token::new(Eof, &slice[..1]),
             b'a'..=b'z' | b'A'..=b'Z' => {
                 let mut end = 1;
                 while end < slice.len() && slice[end].is_ascii_alphanumeric() {
                     end += 1;
                 }
-                Token::new(Word, &slice[..end])
+
+                let token_type = match &slice[..end] {
+                    b"let" => Let,
+                    b"mut" => Mut,
+                    b"fn" => Fn,
+                    _ => Ident,
+                };
+
+                Token::new(token_type, &slice[..end])
             }
             b'0'..=b'9' => {
                 let mut end = 1;
@@ -175,5 +230,68 @@ mod tests {
                 TokenType::Curly(BracketState::Close),
             ],
         );
+    }
+
+    #[test]
+    fn test_a_declare_a_variable() {
+        let input = r"let mut five = 5;";
+
+        let expected = vec![
+            TokenType::Let,
+            TokenType::Mut,
+            TokenType::Ident,
+            TokenType::Assign,
+            TokenType::Integer,
+            TokenType::Semicolon,
+        ];
+
+        test_lexer(input, expected);
+    }
+
+    #[test]
+    #[should_panic(expected = "Unexpected close bracket: }")]
+    fn test_panic_on_uncorrect_brackets() {
+        let input = r"let mut five = 5; }";
+
+        let expected = vec![
+            TokenType::Let,
+            TokenType::Mut,
+            TokenType::Ident,
+            TokenType::Assign,
+            TokenType::Integer,
+            TokenType::Semicolon,
+        ];
+
+        test_lexer(input, expected);
+    }
+
+    #[test]
+    fn test_function() {
+        let input = r"fn add(x: int, y: int) -> int {
+            x + y
+        }";
+
+        let expected = vec![
+            TokenType::Fn,
+            TokenType::Ident,
+            TokenType::Paren(BracketState::Open),
+            TokenType::Ident,
+            TokenType::Colon,
+            TokenType::Ident,
+            TokenType::Comma,
+            TokenType::Ident,
+            TokenType::Colon,
+            TokenType::Ident,
+            TokenType::Paren(BracketState::Close),
+            TokenType::Arrow,
+            TokenType::Ident,
+            TokenType::Curly(BracketState::Open),
+            TokenType::Ident,
+            TokenType::Plus,
+            TokenType::Ident,
+            TokenType::Curly(BracketState::Close),
+        ];
+
+        test_lexer(input, expected);
     }
 }
