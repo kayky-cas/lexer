3d19d50fc5203eeb243c0fcb2fad575184520c77
Kayky Casagrande <kbelleboni@gmail.com>
2023-05-29 22:17:06 -0300

    one step over..

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,10 @@ enum TokenType {
     Semicolon,
     Slash,
     Star,
+    EOF,
 }
 
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Token<'a> {
     token_type: TokenType,
     literal: &'a [u8],
@@ -38,12 +40,16 @@ struct Lexer<'a> {
 }
 
 impl Lexer<'_> {
-    fn new(source: &[u8]) -> Lexer {
+    pub fn new(source: &[u8]) -> Lexer {
         Lexer {
             source,
             position: 0,
             braces_stack: Vec::new(),
         }
+    }
+
+    pub fn validate(&self) -> bool {
+        true
     }
 }
 
@@ -54,15 +60,16 @@ impl<'a> Iterator for Lexer<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.position >= self.source.len() {
-            if !self.braces_stack.is_empty() {
-                panic!("Mismatched brackets");
-            }
             return None;
         }
 
         let slice = &self.source[self.position..];
 
         let token = match slice[0] {
+            b' ' | b'\n' | b'\t' => {
+                self.position += 1;
+                return self.next();
+            }
             b'(' => {
                 let token = Token::new(Paren(BracketState::Open), &slice[..1]);
                 self.braces_stack.push(token.token_type.clone());
@@ -70,12 +77,11 @@ impl<'a> Iterator for Lexer<'a> {
             }
             b')' => {
                 let token = Token::new(Paren(BracketState::Close), &slice[..1]);
-                if let Some(bracket) = self.braces_stack.pop() {
-                    if bracket != token.token_type {
-                        panic!("Mismatched brackets");
-                    }
+                if let Some(Paren(BracketState::Open)) = self.braces_stack.pop() {
+                    token
+                } else {
+                    return None;
                 }
-                token
             }
             b'{' => {
                 let token = Token::new(Brace(BracketState::Open), &slice[..1]);
@@ -84,12 +90,11 @@ impl<'a> Iterator for Lexer<'a> {
             }
             b'}' => {
                 let token = Token::new(Brace(BracketState::Close), &slice[..1]);
-                if let Some(bracket) = self.braces_stack.pop() {
-                    if bracket != token.token_type {
-                        panic!("Mismatched brackets");
-                    }
+                if let Some(Brace(BracketState::Open)) = self.braces_stack.pop() {
+                    token
+                } else {
+                    return None;
                 }
-                token
             }
             b',' => Token::new(Comma, &slice[..1]),
             b'.' => Token::new(Dot, &slice[..1]),
@@ -98,6 +103,7 @@ impl<'a> Iterator for Lexer<'a> {
             b';' => Token::new(Semicolon, &slice[..1]),
             b'/' => Token::new(Slash, &slice[..1]),
             b'*' => Token::new(Star, &slice[..1]),
+            b'\0' => Token::new(EOF, &slice[..1]),
             _ => panic!("Unknown token"),
         };
 
@@ -116,6 +122,7 @@ mod tests {
 
         let tokens: Vec<_> = lexer.map(|token| token.token_type).collect();
 
+        assert_eq!(tokens.last(), Some(&TokenType::EOF));
         assert_eq!(tokens, expected);
     }
 
@@ -125,8 +132,29 @@ mod tests {
             "()",
             vec![
                 TokenType::Paren(BracketState::Open),
-                TokenType::Paren(BracketState::Open),
+                TokenType::Paren(BracketState::Close),
             ],
         );
+    }
+
+    #[test]
+    fn test_lexer_brace() {
+        test_lexer(
+            "{}",
+            vec![
+                TokenType::Brace(BracketState::Open),
+                TokenType::Brace(BracketState::Close),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_faild_bracket() {
+        let lexer = super::Lexer::new("(".as_bytes());
+
+        let tokens: Vec<_> = lexer.map(|token| token.token_type).collect();
+
+        assert_eq!(tokens.last(), Some(&TokenType::EOF));
+        assert_eq!(tokens, vec![TokenType::Paren(BracketState::Open)]);
     }
 }
